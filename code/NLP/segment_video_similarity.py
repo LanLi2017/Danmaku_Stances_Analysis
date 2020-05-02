6ec860e2036cf1518e3e28c5602df8b91d7a2602
Lan Li <lilan.scut@gmail.com>
2020-05-02 02:05:54 -0500

    chunks cluster

--- code/NLP/segment_video_similarity.py
+++ code/NLP/segment_video_similarity.py
@@ -94,27 +94,28 @@ def istest(data):
     plt.savefig('figure/sen_similarity.png')
 
 
-def clustering_window(vector_dan, labels):
+def clustering_window(vector_dan, labels, no_chunks):
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(vector_dan)
+    print(no_chunks)
 
     dbscan = DBSCAN(eps=0.123, min_samples=2, metric='cosine')
     clusters = dbscan.fit_predict(X_scaled)
     plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
     # pprint(clusters.labels_)
-    plt.figure(figsize=(20, 10))
+    plt.figure(figsize=(30, 10))
     plt.scatter(X_scaled[:, 0], X_scaled[:, 1], c=clusters, cmap="plasma")
     for i,txt in enumerate(labels):
         plt.annotate(txt, (X_scaled[:,0][i], X_scaled[:,1][i]))
     plt.xlabel("Feature 0")
     plt.ylabel("Feature 1")
-    plt.savefig('figure/dbscan_cluster_win.png')
-    plt.show()
+    plt.savefig(f'figure/window_subplot/cluster_win{no_chunks}.png')
+    # plt.show()
     return clusters
 
 
 def main():
-    filep = "../combined_data/92725048/combined_92725048.csv"
+    filep = "../combined_data/61437877/combined_61437877.csv"
     column = ['Barrages_original','Showing_time']
     # output_path='danmaku.txt'
     output_path = 'data/61437877'
@@ -123,7 +124,7 @@ def main():
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    n = 50
+    n = 200
     # pandas dataframe : ['danmaku','converted_time']
     df = extract_text(filep, column, data_p)
     danmaku_label = list( df['Barrages_original'])
@@ -148,13 +149,17 @@ def main():
    #  test(split_d)
 
     # DBSCAN : Density-based spatial clustering of applications with noise
-    pprint(dan_labels[0])
-    clustering_window(split_d[20], dan_labels[0])
+    # pprint(dan_labels[0])
+    # clustering_window(split_d[0], dan_labels[0])
+
+    for i in range(len(split_d)):
+        clustering_window(split_d[i], dan_labels[i], i)
     # for vector_dan in split_d:
     #     print(len(vector_dan))
-    #     # distance pairingly
-    #     # dis_metric = cal_distance(vector_dan)
-    #     # label: positive / neural / negative
+    #     clustering_window(vector_dan, dan_labels[0])
+        # distance pairingly
+        # dis_metric = cal_distance(vector_dan)
+        # label: positive / neural / negative
     #     cluster_dan = DBSCAN(min_samples=3, metric='precomputed').fit(distance.pdist(vector_dan, metric='cosine'))
 
 
